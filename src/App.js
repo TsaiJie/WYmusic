f24a555b63b483ae3675e4565f96315f5dd6a29f
tsai <mengjietsai@163.com>
2021-01-22 21:22:46 +0800

    redux的组织方式以及reducer的拆分

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 // 第三方的
 import React, { memo } from 'react';
+import { Provider } from 'react-redux';
 import { renderRoutes } from 'react-router-config';
 import { BrowserRouter as Router } from 'react-router-dom';
 // 功能性的
 import routes from '@/router';
+import store from '@/store';
 // 组件
 import WYAppHeader from '@/components/app-header';
 import WYAppFooter from '@/components/app-footer';
@@ -11,11 +13,13 @@ import WYAppFooter from '@/components/app-footer';
 export default memo(function App() {
   return (
     <div>
-      <Router>
-        <WYAppHeader />
-        {renderRoutes(routes)}
-        <WYAppFooter />
-      </Router>
+      <Provider store={store}>
+        <Router>
+          <WYAppHeader />
+          {renderRoutes(routes)}
+          <WYAppFooter />
+        </Router>
+      </Provider>
     </div>
   );
 });
