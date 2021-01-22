71afe4de041f47fcbd7500e124f8d963298e6259
tsai <mengjietsai@163.com>
2021-01-22 11:20:07 +0800

    header中的选项卡以及样式

--- src/components/app-header/index.js
+++ src/components/app-header/index.js
@@ -1,23 +1,41 @@
 /* eslint-disable jsx-a11y/anchor-has-content */
 import React, { memo } from 'react';
 
-import { NavLink } from 'react-router-dom';
+import { headerLinks } from '@/common/local-data';
 
 import { HeaderWrapper, HeaderLeft, HeaderRight } from './style';
+import { NavLink } from 'react-router-dom';
 
 export default memo(function WYAppHeader() {
+  // 业务代码
+  const showSelectItem = (item, index) => {
+    if (index < 3) {
+      return (
+        <NavLink to={item.link} exact>
+          {item.title} <i className="sprite_01 icon"></i>{' '}
+        </NavLink>
+      );
+    } else {
+      return <a href={item.link}>{item.title}</a>;
+    }
+  };
+
   return (
     <HeaderWrapper>
       <div className="content wrap-v1">
         <HeaderLeft>
           <a href="/" className="logo sprite_01"></a>
-          <NavLink to="/">发现音乐</NavLink>
-          <NavLink to="/mine">我的音乐</NavLink>
-          <NavLink to="friend">我的朋友</NavLink>
+          <div className="select-list">
+            {headerLinks.map((item, index) => {
+              return (
+                <div key={item.title} className={'select-item'}>
+                  {showSelectItem(item, index)}
+                </div>
+              );
+            })}
+          </div>
         </HeaderLeft>
-        <HeaderRight>
-          right
-        </HeaderRight>
+        <HeaderRight>right</HeaderRight>
       </div>
       <div className="divider"></div>
     </HeaderWrapper>
