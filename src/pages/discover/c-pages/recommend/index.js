7df2fadb9fedd6a4a3c5892e9469d49644c26703
tsai <mengjietsai@163.com>
2021-01-23 11:13:50 +0800

    推荐数据通过redux-thunk请求

--- src/pages/discover/c-pages/recommend/index.js
+++ src/pages/discover/c-pages/recommend/index.js
@@ -1,9 +1,24 @@
-import React, { memo } from 'react'
-
-export default memo(function WYRecommend() {
+import React, { memo, useEffect } from 'react';
+import { connect } from 'react-redux';
+import { getTopBannerAction } from './store/actionCreators';
+function WYRecommend(props) {
+  const { getBanners, topBanners } = props;
+  useEffect(() => {
+    getBanners();
+  }, [getBanners]);
   return (
     <div>
-      <h2>WYRecommend</h2>
+      <h2>WYRecommend {topBanners.length}</h2>
     </div>
-  )
-})
+  );
+}
+const mapStateToProps = (state) => ({
+  topBanners: state.recommend.topBanners,
+});
+
+const mapDispatchToProps = (dispatch) => ({
+  getBanners: () => {
+    dispatch(getTopBannerAction());
+  },
+});
+export default connect(mapStateToProps, mapDispatchToProps)(memo(WYRecommend));
---
+++ src/services/recommend.js
@@ -0,0 +1,4 @@
+import request from './request';
+export function getTopBanners() {
+  return request({ url: '/banner' });
+}
