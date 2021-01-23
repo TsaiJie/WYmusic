d13c192f026d50a3250823afaa9e35c250c595f4
tsai <mengjietsai@163.com>
2021-01-23 11:53:50 +0800

    redux hooks 以及useSelector性能优化

--- src/pages/discover/c-pages/recommend/index.js
+++ src/pages/discover/c-pages/recommend/index.js
@@ -1,24 +1,51 @@
 import React, { memo, useEffect } from 'react';
-import { connect } from 'react-redux';
+import { connect, shallowEqual, useDispatch, useSelector } from 'react-redux';
 import { getTopBannerAction } from './store/actionCreators';
+
 function WYRecommend(props) {
-  const { getBanners, topBanners } = props;
+  //  redux hook的使用
+  // 组件和redux关联：获取数据和进行操作
+  // 默认是===比较
+  // 需要使用浅层比较，可以提升性能， 避免无谓的重新渲染
+  const { topBanners } = useSelector(
+    (state) => ({
+      topBanners: state.recommend.topBanners,
+    }),
+    shallowEqual
+  );
+  const dispatch = useDispatch();
+
+  // 发送网络请求
   useEffect(() => {
-    getBanners();
-  }, [getBanners]);
+    dispatch(getTopBannerAction());
+  }, [dispatch]);
   return (
     <div>
-      <h2>WYRecommend {topBanners.length}</h2>
+      <h2>WYRecommend {topBanners.length} </h2>
     </div>
   );
 }
-const mapStateToProps = (state) => ({
-  topBanners: state.recommend.topBanners,
-});
 
-const mapDispatchToProps = (dispatch) => ({
-  getBanners: () => {
-    dispatch(getTopBannerAction());
-  },
-});
-export default connect(mapStateToProps, mapDispatchToProps)(memo(WYRecommend));
+export default memo(WYRecommend);
+
+// function WYRecommend(props) {
+//   const { getBanners, topBanners } = props;
+//   useEffect(() => {
+//     getBanners();
+//   }, [getBanners]);
+//   return (
+//     <div>
+//       <h2>WYRecommend {topBanners.length}</h2>
+//     </div>
+//   );
+// }
+// const mapStateToProps = (state) => ({
+//   topBanners: state.recommend.topBanners,
+// });
+
+// const mapDispatchToProps = (dispatch) => ({
+//   getBanners: () => {
+//     dispatch(getTopBannerAction());
+//   },
+// });
+// export default connect(mapStateToProps, mapDispatchToProps)(memo(WYRecommend));
