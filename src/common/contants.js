f090cf36be92c49073ac5429884f3bb039bbed24
tsai <mengjietsai@163.com>
2021-01-26 21:28:23 +0800

    NewAlbum组件

--- src/common/contants.js
+++ src/common/contants.js
@@ -1,2 +1,4 @@
 export const HOT_RECOMMENDS_LIMIT = 8;
 export const NEW_ALBUM_LIMIT = 10;
+export const NEW_ALBUM_PAGE_NUM = 2;
+export const NEW_ALBUM_PER_PAGE_NUM = 5;
--- src/pages/discover/c-pages/recommend/c-cpns/new-album/index.js
+++ src/pages/discover/c-pages/recommend/c-cpns/new-album/index.js
@@ -1,4 +1,4 @@
-import React, { memo, useEffect } from 'react';
+import React, { memo, useEffect, useRef } from 'react';
 import { Carousel } from 'antd';
 
 import { shallowEqual, useDispatch, useSelector } from 'react-redux';
@@ -6,7 +6,9 @@ import { getNewAlbumAction } from '../../store';
 import { NEW_ALBUM_LIMIT } from '@/common/contants';
 
 import WYThemeHeaderRCM from '@/components/theme-header-rcm';
+import WYAlbumCover from '@/components/album-cover';
 import { NewAlbumWrapper } from './style';
+import { NEW_ALBUM_PER_PAGE_NUM } from '../../../../../../common/contants';
 
 export default memo(function WYNewAlbum() {
   const { newAlbums } = useSelector(
@@ -15,8 +17,9 @@ export default memo(function WYNewAlbum() {
     }),
     shallowEqual
   );
-  console.log(newAlbums);
   const dispatch = useDispatch();
+  // other hooks
+  const pageRef = useRef();
   useEffect(() => {
     dispatch(getNewAlbumAction(NEW_ALBUM_LIMIT));
   }, [dispatch]);
@@ -24,18 +27,40 @@ export default memo(function WYNewAlbum() {
     <NewAlbumWrapper>
       <WYThemeHeaderRCM title="新碟上架" />
       <div className="content">
-        <button className="arrow arrow-left sprite_02"></button>
+        <button
+          className="arrow arrow-left sprite_02"
+          onClick={(e) => pageRef.current.prev()}
+        ></button>
         <div className="album">
-          <Carousel>
-            <div>
-              <h3>1</h3>
-            </div>
-            <div>
-              <h3>2</h3>
-            </div>
+          <Carousel dots={false} ref={pageRef}>
+            {[0, 1].map((item) => {
+              return (
+                <div key={item} className="page">
+                  {newAlbums
+                    .slice(
+                      item * NEW_ALBUM_PER_PAGE_NUM,
+                      (item + 1) * NEW_ALBUM_PER_PAGE_NUM
+                    )
+                    .map((iten) => {
+                      return (
+                        <WYAlbumCover
+                          key={iten.id}
+                          info={iten}
+                          size={100}
+                          width={118}
+                          bgp="-570px"
+                        />
+                      );
+                    })}
+                </div>
+              );
+            })}
           </Carousel>
         </div>
-        <button className="arrow arrow-right sprite_02"></button>
+        <button
+          className="arrow arrow-right sprite_02"
+          onClick={(e) => pageRef.current.next()}
+        ></button>
       </div>
     </NewAlbumWrapper>
   );
