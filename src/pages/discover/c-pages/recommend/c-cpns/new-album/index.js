bd53b4b9af96f9297b9c558b0caaa57718e63e87
tsai <mengjietsai@163.com>
2021-01-26 17:55:40 +0800

    newAlbum基本样式

--- src/pages/discover/c-pages/recommend/c-cpns/new-album/index.js
+++ src/pages/discover/c-pages/recommend/c-cpns/new-album/index.js
@@ -1,24 +1,42 @@
 import React, { memo, useEffect } from 'react';
+import { Carousel } from 'antd';
 
+import { shallowEqual, useDispatch, useSelector } from 'react-redux';
 import { getNewAlbumAction } from '../../store';
-import WYThemeHeaderRCM from '@/components/theme-header-rcm';
-
-import { NewAlbumWrapper } from './style';
-import { useDispatch, useSelector } from 'react-redux';
 import { NEW_ALBUM_LIMIT } from '@/common/contants';
-export default memo(function WYNewAlbum() {
-  const { newAlbums } = useSelector((state) => ({
-    newAlbums: state.recommend.newAlbums,
-  }));
-  console.log(newAlbums)
-  const dispatch = useDispatch();
 
+import WYThemeHeaderRCM from '@/components/theme-header-rcm';
+import { NewAlbumWrapper } from './style';
+
+export default memo(function WYNewAlbum() {
+  const { newAlbums } = useSelector(
+    (state) => ({
+      newAlbums: state.recommend.newAlbums,
+    }),
+    shallowEqual
+  );
+  console.log(newAlbums);
+  const dispatch = useDispatch();
   useEffect(() => {
     dispatch(getNewAlbumAction(NEW_ALBUM_LIMIT));
   }, [dispatch]);
   return (
     <NewAlbumWrapper>
       <WYThemeHeaderRCM title="新碟上架" />
+      <div className="content">
+        <button className="arrow arrow-left sprite_02"></button>
+        <div className="album">
+          <Carousel>
+            <div>
+              <h3>1</h3>
+            </div>
+            <div>
+              <h3>2</h3>
+            </div>
+          </Carousel>
+        </div>
+        <button className="arrow arrow-right sprite_02"></button>
+      </div>
     </NewAlbumWrapper>
   );
 });
