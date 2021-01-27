0977caba938a921e42487d246ea5eb15108eb206
tsai <mengjietsai@163.com>
2021-01-27 09:56:19 +0800

    榜单完成

--- src/pages/discover/c-pages/recommend/c-cpns/recommend-rank/index.js
+++ src/pages/discover/c-pages/recommend/c-cpns/recommend-rank/index.js
@@ -1,10 +1,17 @@
 import React, { memo, useEffect } from 'react';
 
+
+import { useDispatch, useSelector } from 'react-redux';
+import { getTopListAction } from '../../store/actionCreators';
+import WYTopRanking from '@/components/top-ranking';
 import WYThemeHeaderRCM from '@/components/theme-header-rcm';
 import { RecommendRankWrapper } from './style';
-import { useDispatch } from 'react-redux';
-import { getTopListAction } from '../../store/actionCreators';
 export default memo(function WYRecommendRank() {
+  const { upRanking, newRanking, originRanking} = useSelector(state => ({
+    upRanking: state.recommend.upRanking,
+    newRanking: state.recommend.newRanking,
+    originRanking: state.recommend.originRanking
+  }))
   const dispatch = useDispatch();
   useEffect(() => {
     dispatch(getTopListAction(0));
@@ -14,6 +21,11 @@ export default memo(function WYRecommendRank() {
   return (
     <RecommendRankWrapper>
       <WYThemeHeaderRCM title="榜单" />
+      <div className="tops">
+        <WYTopRanking info ={upRanking}/>
+        <WYTopRanking info ={newRanking}/>
+        <WYTopRanking info ={originRanking}/>
+      </div>
     </RecommendRankWrapper>
   );
 });
